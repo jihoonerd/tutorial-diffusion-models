f8e077d0d90d3737666fc87e28bb2b456b9fa3f6
Jihoon Kim <jihoonerd@outlook.com>
2022-05-16 18:21:00 +0900

    Add ema model

---
+++ src/models/ema.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from copy import deepcopy
+import torch
+
+class EMAModel(nn.Module):
+    """Exponentila Moving Average Model"""
+
+    def __init__(self, model, decay=0.9999):
+        super().__init__()
+
+        self.module = deepcopy(model)
+        self.module.eval()
+        self.decay = decay
+
+    def update(self, model):
+        with torch.no_grad():
+            for ema_w, model_w in zip(self.module.state_dict().values(), model.state_dict().values()):
+                ema_w.copy_(self.decay * ema_w + (1. - self.decay) * model_w)

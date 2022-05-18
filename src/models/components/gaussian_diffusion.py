497bf7caa26d9ec7ebecf0799bfc9e766f8fb2c5
Jihoon Kim <jihoonerd@outlook.com>
2022-05-18 15:35:44 +0900

    Black formatting

--- src/models/components/gaussian_diffusion.py
+++ src/models/components/gaussian_diffusion.py
@@ -311,10 +311,10 @@ class GaussianDiffusion:
             else:
                 pred_xstart = process_xstart(
                     self._predict_xstart_from_eps(x_t=x, t=t, eps=model_output)
-                ) # Make model to predict epsilon. Following part does not have learnable parameters.
+                )  # Make model to predict epsilon. Following part does not have learnable parameters.
             model_mean, _, _ = self.q_posterior_mean_variance(
                 x_start=pred_xstart, x_t=x, t=t
-            )  
+            )
         else:
             raise NotImplementedError(self.model_mean_type)
 
--- src/models/ema.py
+++ src/models/ema.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from copy import deepcopy
 import torch
 
+
 class EMAModel(nn.Module):
     """Exponentila Moving Average Model"""
 
@@ -14,5 +15,7 @@ class EMAModel(nn.Module):
 
     def update(self, model):
         with torch.no_grad():
-            for ema_w, model_w in zip(self.module.state_dict().values(), model.state_dict().values()):
-                ema_w.copy_(self.decay * ema_w + (1. - self.decay) * model_w)
+            for ema_w, model_w in zip(
+                self.module.state_dict().values(), model.state_dict().values()
+            ):
+                ema_w.copy_(self.decay * ema_w + (1.0 - self.decay) * model_w)

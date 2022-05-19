13552666171a10cf4bbe60bacd31874737aeced9
Jihoon Kim <jihoonerd@outlook.com>
2022-05-19 16:24:33 +0900

    Refactor attribute of ema

--- src/models/ema.py
+++ src/models/ema.py
@@ -9,13 +9,13 @@ class EMAModel(nn.Module):
     def __init__(self, model, decay=0.9999):
         super().__init__()
 
-        self.module = deepcopy(model)
-        self.module.eval()
+        self.model = deepcopy(model)
+        self.model.eval()
         self.decay = decay
 
     def update(self, model):
         with torch.no_grad():
             for ema_w, model_w in zip(
-                self.module.state_dict().values(), model.state_dict().values()
+                self.model.state_dict().values(), model.state_dict().values()
             ):
                 ema_w.copy_(self.decay * ema_w + (1.0 - self.decay) * model_w)
